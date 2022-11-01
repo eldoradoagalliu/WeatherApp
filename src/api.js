6d318320a2f4e2240bafd3aa5a84b03db47a6889
Eldorado <eldoradoagalliu@gmail.com>
2022-11-01 12:13:30 +0100

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+//Get Api from RapidAPI
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'aa51873e08mshf30adfdeefcce6fp1e00b6jsnae61cef0f3f4',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d26f8041c07e58e3c9ec0fa019f34599";
